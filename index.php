483a1a3f6acacdf8f8ff954f114943e5437bfa01
Gabriel Fachineli <gabrielfachineli200023@gmail.com>
2021-03-22 23:47:25 -0300

    agr sim esta resolvido os conflitos

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 
-<<<<<<< HEAD
 	echo "Ola mundo 456!";
 
-	echo "Ola Udemy!"
-=======
-	echo "Ola mundo 123!";
-
-	echo "Ola Hcode!";
->>>>>>> 4b690f61c83576c3303d09c0680ddd995347803e
+	echo "Ola Udemy e Hcode!"
 
 ?>
